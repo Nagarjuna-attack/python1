02f2d291b5d5591045805a0208b084855460ec75
Muhafandi <garamgudang1917@gmail.com>
2021-07-14 04:07:57 +0800

    asdf

--- fungsi.py
+++ fungsi.py
@@ -1,5 +1,4 @@
-def great_function(name="Admin"):
-	print("Selamat Datang "+name)
+def great_function(name="Admin",age=20):
+	print("Selamat Datang "+name+" Umur = "+str(age))
 
-great_function()
-	
+great_function('Afandi',40)
---
+++ return.py
@@ -0,0 +1,8 @@
+def kembali(num1,num2):
+	return num1+num2
+
+
+num1 = int(input("Masukkan angka pertama : "))
+num2 = int(input("Masukkan angka kedua : "))
+print(kembali(num1,num2))
+
